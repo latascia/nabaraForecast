1c058dade1502686c61b6a8d7f506c2bf1c13b4a
Nata <nabarata@gmail.com>
2017-10-15 22:36:42 +0300

    It still doesn't work yet (bt I'll d m bst)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
+import request.RequestBuilder;
+
 /**
  * Created by Nata on 28.09.17.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("150:017".substring(4));
+        RequestBuilder.getForecast();
     }
 }
